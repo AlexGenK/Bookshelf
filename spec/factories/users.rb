9054082118f532e6a7a5e3a72bacd6f1f94549b4
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-03-03 23:12:09 +0200

    Changed users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
-  factory :superadmin, class: User do
-    username              "superadmin"
+  factory :super_admin, class: User do
+    username              "super_admin"
     email                 "superadmin@mail.no"
+    password              "123456"
     superadmin_role       true
     admin_books_role      false
     admin_categories_role false
@@ -10,6 +11,7 @@ FactoryBot.define do
   factory :books_admin, class: User do
     username              "books_admin"
     email                 "books_admin@mail.no"
+    password              "123456"
     superadmin_role       false
     admin_books_role      true
     admin_categories_role false
@@ -18,6 +20,7 @@ FactoryBot.define do
   factory :categories_admin, class: User do
     username              "categories_admin"
     email                 "categories_admin@mail.no"
+    password              "123456"
     superadmin_role       false
     admin_books_role      false
     admin_categories_role true
