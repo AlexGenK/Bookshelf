ddcc7b676ec4301f2d1bca9bfd2278bc21046746
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-03-04 18:06:31 +0200

    Refactored feature test for the home controller

--- spec/features/home_features_spec.rb
+++ spec/features/home_features_spec.rb
@@ -7,15 +7,15 @@ feature 'View bookshelf', type: :feature do
     visit root_path
   end
 
-  context 'when Visitor is logged out' do
-    scenario 'Visitor is viewing bookshelf' do
-      expect(page).to have_content @category.name
-      expect(page).to have_content @book.category.name
-      expect(page).to have_content @book.title
-      expect(page).to have_css('.category-card', count: 2)
-      expect(page).to have_css('.book-card', count: 1)
-    end
+  scenario 'Visitor is viewing bookshelf' do
+    expect(page).to have_content @category.name
+    expect(page).to have_content @book.category.name
+    expect(page).to have_content @book.title
+    expect(page).to have_css('.category-card', count: 2)
+    expect(page).to have_css('.book-card', count: 1)
+  end
 
+  context 'when Visitor is logged out' do
     scenario 'Visitor cannot access to admin area' do
       expect(page).to_not have_content 'Admin area'
     end
@@ -29,16 +29,7 @@ feature 'View bookshelf', type: :feature do
       fill_in 'Password', with: @user.password
       click_button 'Log in'
     end
-
-    scenario 'Visitor is viewing bookshelf' do
-      expect(page).to have_content @category.name
-      expect(page).to have_content @book.category.name
-      expect(page).to have_content @book.title
-      expect(page).to have_css('.category-card', count: 2)
-      expect(page).to have_css('.book-card', count: 1)
-      expect(page).to have_content 'Admin area'
-    end
-
+    
     scenario 'Visitor can access to admin area' do
       expect(page).to have_content 'Admin area'
     end
