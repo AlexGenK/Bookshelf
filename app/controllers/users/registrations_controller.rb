2923b5c400d30571d093442a189005f19e198b93
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-03-11 15:58:14 +0200

    Renamed redirect_unless_admin to redirect_unless_signed_in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_user!, :redirect_unless_admin, only: [:new, :create]
+  before_action :authenticate_user!, :redirect_unless_signed_in, only: [:new, :create]
   skip_before_action :require_no_authentication
 
   private
 
-  def redirect_unless_admin
+  def redirect_unless_signed_in
     unless user_signed_in?
       flash[:alert] = "Only registered users can do that"
       redirect_to root_path
